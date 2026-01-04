2694cd56e347d06f83cd74913db659dea778d5d3
kamali-05 <kamalichellapandi@gmail.com>
2026-01-05 01:06:01 +0530

    Add src folder with arrays and sorting subfolders and initial Java files

---
+++ src/arrays/PrintArray.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        for (int j : arr) {
+            System.out.println(j);
+        }
+    }
+
+}
