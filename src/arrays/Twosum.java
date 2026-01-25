b6a659350d3b57115a5c75d31aa79f6cabcc08ef
kamali-05 <kamalichellapandi@gmail.com>
2026-01-26 00:11:14 +0530

    Add Twosum.java implementation

--- src/arrays/Twosum.java
+++ src/arrays/Twosum.java
@@ -1,4 +1,27 @@
 package arrays;
-
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Arrays;
 public class Twosum {
+    public static int[] FindTheBest(int[] nums, int target){
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int need=target-nums[i];
+            if(map.containsKey(need)){
+                return new int[]{map.get(need),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        System.out.println(Arrays.toString(FindTheBest(nums,target)));
+    }
 }
